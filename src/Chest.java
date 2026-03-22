e79a6c2ecacea0b500bf0d34cd32ef9ff05e78ff
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-03-22 17:02:04 +0000

    stub methods for the class yet

--- src/Chest.java
+++ src/Chest.java
@@ -13,6 +13,45 @@ public class Chest extends WorldStructure {
     public Chest(double x, double y) {
         super(x, y, "Chest");
         this.contents = new Item[10]; //it will default storage capacity
-        this.opened = false; //also closed by defaut 
-    }
+        this.opened = false; //also close by defaut 
+    }//methods with stubs
+    
+    //method  when a player interacts with the chest.
+
+  @Override
+  public void interact(Player player) {
+   
+      open(player);
+  }
+
+
+   // method that toggles the chest state to opened.
+  public void open(Player player) {
+   
+  }
+
+
+   //metjod to remove item from a specific slot in the chest
+ 
+  public Item takeItem(int index, Player player) {
+
+      return null;
+  }
+
+
+   //method thatchecks if the player is close enough to interact with chest
+ 
+  @Override
+  public boolean isPlayerInRange(Player player) {
+
+      return false;
+  }
+
+ 
+   //method -draws chest sprite on the screen
+  @Override
+  public void render(Graphics2D g2) {
+     
+  }
+}
 }
