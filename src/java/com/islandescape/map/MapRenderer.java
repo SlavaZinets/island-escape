305b2a3e9d2b723b173e9b6c39b30823042a73c9
Maksym926 <maxchechotkin@gmail.com>
2026-03-23 10:07:15 +0000

    MapRender: implement methods for tileset loading, checking whether tile is empty, finding the source coordinates of the tile

--- src/java/com/islandescape/map/MapRenderer.java
+++ src/java/com/islandescape/map/MapRenderer.java
@@ -13,13 +13,31 @@ public class MapRenderer {
     private final int offset;
     private final int columns;
     private final int tileSize;
-    private BufferedImage image;
+    private BufferedImage tilesetImage;
 
 
     public MapRenderer( int offset, int columns, int tileSize) {
         this.offset = offset;
         this.columns = columns;
         this.tileSize = tileSize;
+    }
+
+    // load tileset image
+    public void loadTileset(String path) throws IOException{
+        tilesetImage = ImageIO.read(new File(path));
+    }
+    // check if the tile is empty
+    public boolean isEmpty(int tileId){
+        return tileId == 0;
+    }
+
+    // get the x coordinate of the tile in the tileset image
+    public int getTileSourceX(int tileId){
+        return (tileId - offset) % columns * tileSize;
+    }
+    // get the y coordinate of the tile in the tileset image
+    public int getTileSourceY(int tileId){
+        return (tileId - offset) / columns * tileSize;
     }
 
 }
