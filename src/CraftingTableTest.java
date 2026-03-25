b04737ea8058f73bc62c6dc097f1067ddc70b8dc
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-03-25 10:44:04 +0000

    Implemented basic interact logic to open the chest

---
+++ src/CraftingTableTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CraftingTableTest {
+    @Test   public void interact() {
+
+    }
+@Test
+    public void isPlayerNearby() {
+
+
+    }
+
+
+    @Test
+    public void isPlayerInRange() {
+
+
+    }
+}
