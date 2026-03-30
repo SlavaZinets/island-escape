c64a31b80ffc5a14893979bb226da8dcc7d2c1c4
SlavaZinets <slavazinets4@gmail.com>
2026-03-30 15:54:49 +0100

    Add Direction class

---
+++ src/classes/Direction.java
@@ -0,0 +1,31 @@
+package classes;
+
+import java.awt.Point;
+
+public class Direction {
+
+    private Point direction;
+
+    public Direction(int x, int y) {
+    }
+
+    public Point getDirection() {
+        return null;
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public void setDirection(int x, int y) {
+    }
+
+    public Direction normalize() {
+        return null;
+    }
+
+}
