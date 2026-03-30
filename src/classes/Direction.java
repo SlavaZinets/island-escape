969db3be76916231e774c6aff644d09f0f988570
SlavaZinets <slavazinets4@gmail.com>
2026-03-30 16:37:23 +0100

    Develop methods in Direction class. Direction passes all tests

--- src/classes/Direction.java
+++ src/classes/Direction.java
@@ -1,33 +1,46 @@
 package classes;
 
-import java.awt.Point;
-
 public class Direction {
 
     private int x;
     private int y;
 
     public Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+        normalize();
     }
 
     public int getX() {
-        return 0;
+        return x;
     }
 
     public int getY() {
-        return 0;
+        return y;
     }
 
     public void setX(int x) {
+        this.x = x;
+        normalize();
     }
-    public void setY(int y) {}
+
+    public void setY(int y) {
+        this.y = y;
+        normalize();
+    }
 
     public void setDirection(int x, int y) {
-
+        this.x = x;
+        this.y = y;
+        normalize();
     }
 
-    private void normalize () {
+    private void normalize() {
+        Integer newX = this.x;
+        Integer newY = this.y;
 
+        this.x = newX.compareTo(0);
+        this.y = newY.compareTo(0);
     }
 
 }
