94d0e55d509826330f812e0e9b3b78bf2e7b4dc3
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-03-30 14:53:44 +0100

    made method getLightRadius with no test as nothing special to test just if fireplace is burning - make the radius for light of 200 pxls, might cnange in future

--- src/Chest.java
+++ src/Chest.java
@@ -10,9 +10,33 @@ public class Chest extends WorldStructure {
     //constructor
     public Chest(double x, double y) {
         super(x, y, "Chest");
-        this.contents = new Item[10]; //it will default storage capacity
+        this.contents = new Item[25]; //it will default storage capacity
         this.opened = false; //also close by defaut
-        this.contents[0] = new Item("stone", "Custom");//put the element into chest to test
+
+        this.contents[0] = new Item("Stone", "Primary Resources");//stone
+        this.contents[1] = new Item("Stone", "Primary Resources");
+        this.contents[2] = new Item("Stone", "Primary Resources");
+        this.contents[3] = new Item("Stone", "Primary Resources");
+        this.contents[4] = new Item("Vines", "Primary Resources");//vines
+        this.contents[5] = new Item("Vines", "Primary Resources");
+        this.contents[6] = new Item("Vines", "Primary Resources");
+        this.contents[7] = new Item("Vines", "Primary Resources");
+        this.contents[8] = new Item("Vines", "Primary Resources");
+        this.contents[9] = new Item("Vines", "Primary Resources");
+        this.contents[10] = new Item("Banana", "Food");//food
+        this.contents[11] = new Item("Banana", "Food");
+        this.contents[12] = new Item("Banana", "Food");
+        this.contents[13] = new Item("Banana", "Food");
+        this.contents[14] = new Item("Coconut", "Food");
+        this.contents[15] = new Item("Coconut", "Food");
+        this.contents[16] = new Item("Tropical Tree Leaves", "Primary Resources");//leaves
+        this.contents[17] = new Item("Tropical Tree Leaves", "Primary Resources");
+        this.contents[18] = new Item("Tropical Tree Leaves", "Primary Resources");
+        this.contents[19] = new Item("Tropical Tree Leaves", "Primary Resources");
+        this.contents[20] = new Item("Wood", "Primary Resources");//wood
+        this.contents[21] = new Item("Wood", "Primary Resources");
+        this.contents[22] = new Item("Wood", "Primary Resources");
+        this.contents[23] = new Item("Wood", "Primary Resources");
     }
     
     //method when a player interacts with the chest.
