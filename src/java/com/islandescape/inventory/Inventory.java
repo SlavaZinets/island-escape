a2efc0eed22a036dc58de1b940f3315884693593
Maksym926 <maxchechotkin@gmail.com>
2026-04-02 00:26:14 +0100

    Inventory: Add empty stubs

--- src/java/com/islandescape/inventory/Inventory.java
+++ src/java/com/islandescape/inventory/Inventory.java
@@ -3,5 +3,27 @@ package com.islandescape.inventory;
 import com.islandescape.item.Item;
 
 public class Inventory {
+    Item[] slots;
+    int capacity;
+
+
+    public boolean addItem(Item item){
+        return true;
+    }
+    public boolean removeItem(Item item){
+        return true;
+    }
+    public boolean isFull(){
+        return true;
+    }
+    public Item[] getItems(){
+        return slots;
+    }
+    public boolean hasItem(Item item, int amount){
+        return true;
+    }
+    public int getItemCount(Item item){
+        return 0;
+    }
 
 }
