a20ff69b61c6faaa6ee3befd72159c1d4a9f133a
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-08 16:40:01 +0100

    made stub method interact() of tree class and it fails the test

--- src/java/com/islandescape/structures/Tree.java
+++ src/java/com/islandescape/structures/Tree.java
@@ -1,8 +1,15 @@
 package com.islandescape.structures;
 
-public class Tree {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public Tree(String name) {
+public class Tree extends NaturalResource{
+
+    public Tree(double x, double y, String name) {
+        super(x, y, "tree", 2, "Tree");
+    }
+
+    @Override
+    public void interact(double playerx, double playery) {
 
     }
 }
--- src/test/java/com/islandescape/structure/TreeTest.java
+++ src/test/java/com/islandescape/structure/TreeTest.java
@@ -1,10 +1,14 @@
 package com.islandescape.structure;
 
 import com.islandescape.structures.Tree;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class TreeTest {
     @Test
-    public void setTestStone() {
 
+    public void interact() {
+        Tree tree = new Tree(50.0, 50.0, "Oak Tree");
+        tree.interact(50.0, 50.0);
+        assertEquals(1, tree.getHealth(), "health should be decreased");
     }
 }
