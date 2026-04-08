6e9bddf19fb3cce23e084270a461b632027be10f
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-08 17:02:57 +0100

    Created class for Vines tp get materail vine and made test class

---
+++ src/java/com/islandescape/structures/VinesPlace.java
@@ -0,0 +1,15 @@
+package com.islandescape.structures;
+
+//vines place to get vine material
+public class VinesPlace extends NaturalResource {
+
+    public VinesPlace(double x, double y, String name) {
+        super(x, y, "vine", 2, "Vines");
+    }
+
+    @Override
+    public void interact(double playerx, double playery) {
+//stub
+    }
+}
+
---
+++ src/test/java/com/islandescape/structure/VinesTest.java
@@ -0,0 +1,12 @@
+package com.islandescape.structure;
+
+import com.islandescape.structures.VinesPlace;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VinesTest {
+    @Test
+    public void interact() {
+
+    }
+}
