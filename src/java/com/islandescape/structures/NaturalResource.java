e45757724e18ae753581d3bd3672e09d6d80c8ec
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-08 13:21:15 +0100

    Added new class Naturalresources that will be used for being parent of specific resources such as stone, tree...

---
+++ src/java/com/islandescape/structures/NaturalResource.java
@@ -0,0 +1,4 @@
+package java.com.islandescape.structures;
+//this will be abstract class from parent class worldstrucure for creating resources such as stone, tree, etc
+public class NaturalResource {
+}
