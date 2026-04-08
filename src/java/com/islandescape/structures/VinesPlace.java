231a9a80a9027e0a697afbed4454b4752e4eeafb
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-08 17:20:41 +0100

    implemented the method interact on VIne class

--- src/java/com/islandescape/structures/VinesPlace.java
+++ src/java/com/islandescape/structures/VinesPlace.java
@@ -4,12 +4,25 @@ package com.islandescape.structures;
 public class VinesPlace extends NaturalResource {
 
     public VinesPlace(double x, double y, String name) {
-        super(x, y, "vine", 2, "Vines");
+        super(x, y, "vine", 1, "Vines");
     }
 
     @Override
     public void interact(double playerx, double playery) {
-//stub
+        double distance = Math.sqrt(Math.pow(this.x - playerx, 2) + Math.pow(this.y - playery, 2));
+//distance cal
+        if (distance <= 40) {
+            if (this.health > 0) {
+                 this.health--; //
+                  System.out.println("Cutting vines");
+
+                if (this.health == 0) {
+                    System.out.println("material got: " + this.itemName);
+                }
+            }
+        } else {
+            System.out.println("Cannot reach");
+        }
     }
 }
 
