a9b601fdcafe9c89e465a378d0ee5d412b17adbe
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-08 16:27:27 +0100

    implemented interact() of stone class and it passes the test

--- src/java/com/islandescape/structures/Stone.java
+++ src/java/com/islandescape/structures/Stone.java
@@ -9,6 +9,21 @@ public class Stone extends NaturalResource {
 
     @Override
     public void interact(double playerx, double playery) {
+        //calculate distance
+        double distance = Math.sqrt(Math.pow(this.x - playerx, 2) + Math.pow(this.y - playery, 2));
+
+        if (distance <= 40) {
+            if (this.health > 0) {
+                this.health--; //decrease the value of health whime minning
+                 System.out.println("Mining:" + this.health);
+
+                if (this.health == 0) {
+                    System.out.println("You mined: " + this.itemName);
+                }//show what material is mined
+            }
+        } else {
+            System.out.println("too far");
+        }
 
     }
 }
