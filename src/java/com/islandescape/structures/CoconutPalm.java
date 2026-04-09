7e14369ff264f02a2327af6105360ea17dbed3f3
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-09 14:08:30 +0100

    made class CocoonutPalm for coconuts with stub

---
+++ src/java/com/islandescape/structures/CoconutPalm.java
@@ -0,0 +1,11 @@
+package com.islandescape.structures;
+
+public class CoconutPalm extends NaturalResource{
+    public CoconutPalm(double x, double y, String name) {
+        super(x, y, name, 2, "Coconut");
+    }
+    @Override
+    public void interact(double playerx, double playery) {
+
+    }
+}
