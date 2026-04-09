bad55039d6ddb2ed6dbe060a54254b2a4eec9fae
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-09 14:27:46 +0100

    imeplemnted and it passes rn

--- src/java/com/islandescape/structures/BananaTree.java
+++ src/java/com/islandescape/structures/BananaTree.java
@@ -8,6 +8,20 @@ public class BananaTree extends NaturalResource {
 
     @Override
     public void interact(double playerx, double playery) {
+        double distance = Math.sqrt(Math.pow(this.x - playerx, 2) + Math.pow(this.y - playery, 2));
 
+        if (distance <= 40) {
+            if (this.health > 0) {
+                this.health--;
+                System.out.println("collect bananas" + this.health);
+
+                if (this.health == 0) {
+                    System.out.println("You got: " + this.itemName);
+                }
+            }
+        } else {
+            System.out.println("Too far from bananas");
+        }
     }
+
 }
