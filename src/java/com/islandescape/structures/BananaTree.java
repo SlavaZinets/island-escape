d6b52cc1e61bcc0ae91aab2a9f4e4b59a1d44468
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-09 14:26:35 +0100

    made class for bananas and test it fails

---
+++ src/java/com/islandescape/structures/BananaTree.java
@@ -0,0 +1,13 @@
+package com.islandescape.structures;
+//class for bananas
+public class BananaTree extends NaturalResource {
+
+    public BananaTree(double x, double y, String name) {
+        super(x, y, name, 1, "Banana");
+    }
+
+    @Override
+    public void interact(double playerx, double playery) {
+
+    }
+}
