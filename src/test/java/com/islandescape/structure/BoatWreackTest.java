78c7ae18492f79c3b14ed549fdae6dbe03bb6445
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-10 19:58:32 +0100

    added tests for methods for boat

---
+++ src/test/java/com/islandescape/structure/BoatWreackTest.java
@@ -0,0 +1,13 @@
+package com.islandescape.structure;
+
+import com.islandescape.structures.BoatWreck;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class BoatWreackTest {
+    @Test
+    public void addPart() {}
+
+
+    @Test
+    public void interact() {}
+}
