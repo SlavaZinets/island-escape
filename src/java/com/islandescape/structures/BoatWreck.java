807e87edfb900fdab4cb26ca3603d753dd33609c
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-10 22:19:01 +0100

    added all required parts for full repair of boat and amount, added method getMissingParts()

--- src/java/com/islandescape/structures/BoatWreck.java
+++ src/java/com/islandescape/structures/BoatWreck.java
@@ -1,13 +1,19 @@
 package com.islandescape.structures;
+
 import java.util.ArrayList;
 import java.util.List;
+
 //this class will be structure of boatwreck wich we will need to repair
 public class BoatWreck extends WorldStructure{
-    private List<String> fixedParts; //making the list for storing all parts crafted from player fpr repairing the boat
+    private List<String> fixedParts; //making list for storing all parts crafted from player fpr repairing the boat
     private final List<String> requiredParts;//the full list of repairs needed for boat
-    public BoatWreck(double x, double y, String name, List<String> requiredParts) {
+    public BoatWreck(double x, double y, String name) {
         super(x, y, "Boat Wreck Site");
-        this.requiredParts = requiredParts;
+        this.requiredParts = List.of(  //initialize required repairs and their amount
+                "Mast", "Frame", "Sail", "Rudder",
+                "Fittings", "Fittings", "Fittings", "Fittings", "Fittings",
+                "Planks", "Planks", "Planks", "Planks", "Planks", "Planks", "Planks", "Planks"
+        );
         this.fixedParts = new ArrayList<>();
     }
     public void addPart(String partName) {}//adding element to boat
@@ -15,6 +21,10 @@ public class BoatWreck extends WorldStructure{
     public boolean isFullyRepaired() {//check for is the repair complete fully or not
         return false;
     }
+    public List<String> getMissingParts() {//get all  of remaining requird  repairs
+        return new ArrayList<>();
+    }
+
     @Override
     public void interact(double playerx, double playery) {}
 
--- src/test/java/com/islandescape/structure/BoatWreackTest.java
+++ src/test/java/com/islandescape/structure/BoatWreackTest.java
@@ -2,12 +2,17 @@ package com.islandescape.structure;
 
 import com.islandescape.structures.BoatWreck;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 public class BoatWreackTest {
     @Test
     public void addPart() {}
 
+    @Test
+    public void getMissingParts() {
 
+    }
     @Test
     public void interact() {}
 }
