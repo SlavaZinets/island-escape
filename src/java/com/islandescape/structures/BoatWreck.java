2a6481f8d9a8bbb40a9091ffe38b6e9fb9431684
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-10 19:53:10 +0100

    come up with appropraite methods o boatwreck class and added as stubs and also added list called required parts so to have specific parts that are needed to pe repaired

--- src/java/com/islandescape/structures/BoatWreck.java
+++ src/java/com/islandescape/structures/BoatWreck.java
@@ -4,9 +4,16 @@ import java.util.List;
 //this class will be structure of boatwreck wich we will need to repair
 public class BoatWreck extends WorldStructure{
     private List<String> fixedParts; //making the list for storing all parts crafted from player fpr repairing the boat
-    public BoatWreck(double x, double y, String name) {
+    private final List<String> requiredParts;//the full list of repairs needed for boat
+    public BoatWreck(double x, double y, String name, List<String> requiredParts) {
         super(x, y, "Boat Wreck Site");
+        this.requiredParts = requiredParts;
         this.fixedParts = new ArrayList<>();
+    }
+    public void addPart(String partName) {}//adding element to boat
+
+    public boolean isFullyRepaired() {//check for is the repair complete fully or not
+        return false;
     }
     @Override
     public void interact(double playerx, double playery) {}
