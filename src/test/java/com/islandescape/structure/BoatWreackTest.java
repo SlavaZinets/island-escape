b4b136a3cd36a6a9b8491beffe99e2df03da362b
Vladyslav Hordiienko <vladyslav.hordiienko@student.griffith.ie>
2026-04-11 11:23:32 +0100

    made test for addPart() - it fails

--- src/test/java/com/islandescape/structure/BoatWreackTest.java
+++ src/test/java/com/islandescape/structure/BoatWreackTest.java
@@ -7,7 +7,13 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 public class BoatWreackTest {
     @Test
-    public void addPart() {}
+    public void addPart() {
+        BoatWreck boat = new BoatWreck(0, 0, "Boat Wreck Site");
+        assertEquals(17, boat.getMissingParts().size());//we should have 17 part by default
+        boat.addPart("Mast");
+        assertEquals(16, boat.getMissingParts().size());//16 after adding the elemnt
+        assertFalse(boat.getMissingParts().contains("Mast"));
+    }
 
     @Test
     public void getMissingParts() {
