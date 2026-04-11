c6d0de1c5a49ce0e6aad10c7d7493f3760373b0e
Maksym926 <maxchechotkin@gmail.com>
2026-04-11 17:15:30 +0100

    Main: add support for multiple players and integrate test items with inventory

--- src/java/com/islandescape/Main.java
+++ src/java/com/islandescape/Main.java
@@ -2,6 +2,10 @@ package com.islandescape;
 
 import com.islandescape.core.GamePanel;
 import com.islandescape.input.GameKeyHandler;
+import com.islandescape.item.Item;
+import com.islandescape.item.ItemCategory;
+import com.islandescape.item.ItemType;
+import com.islandescape.player.Player;
 import javax.swing.SwingUtilities;
 import com.islandescape.window.GameWindow;
 import com.islandescape.map.MapLoader;
@@ -15,14 +19,28 @@ public class Main {
 		// 2. Create the renderer
 		MapRenderer renderer = new MapRenderer();
 
-		// 3. Create the panel that draws the map
-		GamePanel panel = new GamePanel(map, renderer);
+		// 3. Create players
+		Player player1 = new Player("Player 1", 1, 100, 100);
+		Player player2 = new Player("Player 2", 2, 200, 100);
+
+		// test items for Player 1
+		player1.getInventory().addItem(new Item(ItemType.WOOD, ItemCategory.PRIMARY_RESOURCE, "Wood", "A piece of wood", 5));
+		player1.getInventory().addItem(new Item(ItemType.STONE, ItemCategory.PRIMARY_RESOURCE, "Stone", "A solid stone", 3));
+
+
+		// 4. Create the panel that draws the map
+		GamePanel panel = new GamePanel(map, renderer, player1, player2);
 
 
 
-		// 4. Create the window fullscreen and add the panel
+		// 5. Register key handler
+		panel.setFocusable(true);
+		panel.addKeyListener(new GameKeyHandler(panel));
+
+		// 6. Create the window fullscreen and add the panel
 		GameWindow window = new GameWindow("Island Escape");
 		window.add(panel);
 		window.setVisible(true);
+		panel.requestFocusInWindow();
     }
 }
