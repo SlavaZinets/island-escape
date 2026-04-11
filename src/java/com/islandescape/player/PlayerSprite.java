67b1a48d5c1879c8e966294e65b322bd93534588
SlavaZinets <slavazinets4@gmail.com>
2026-04-11 11:10:33 +0100

    create PlayerSprite.java with stubs

---
+++ src/java/com/islandescape/player/PlayerSprite.java
@@ -0,0 +1,37 @@
+package com.islandescape.player;
+
+import java.awt.image.BufferedImage;
+
+public class PlayerSprite {
+
+    public static final int COLS = 6;
+    public static final int ROWS = 5;
+
+    public PlayerSprite(String path) {
+    }
+
+    public int getImageWidth() {
+        return 0;
+    }
+
+    public int getImageHeight() {
+        return 0;
+    }
+
+    public int getFrameWidth() {
+        return 0;
+    }
+
+    public int getFrameHeight() {
+        return 0;
+    }
+
+    public BufferedImage getFrame(int row, int col) {
+
+        return null;
+    }
+
+    public BufferedImage getFlippedFrame(int row, int col) {
+        return null;
+    }
+}
