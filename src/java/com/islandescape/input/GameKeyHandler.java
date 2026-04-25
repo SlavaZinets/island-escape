2bf8299279b46577693b740f180aa33bb3d11394
SlavaZinets <slavazinets4@gmail.com>
2026-04-25 23:09:36 +0100

    Modify GameKeyHandler.java: add key listeners for up and down arrows for main menu. Block other buttons when the game is in main menu state

--- src/java/com/islandescape/input/GameKeyHandler.java
+++ src/java/com/islandescape/input/GameKeyHandler.java
@@ -31,6 +31,11 @@ public class GameKeyHandler implements KeyListener {
     private boolean boatRepairToggled = false;
     private boolean boardKeyPressed = false;
 
+    // Main menu navigation edges
+    private boolean menuUp = false;
+    private boolean menuDown = false;
+    private boolean menuConfirm = false;
+
     private final GamePanel gamePanel;
 
     public GameKeyHandler(GamePanel gamePanel) {
@@ -40,6 +45,23 @@ public class GameKeyHandler implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
+
+        // Main menu state owns navigation keys and blocks all other input
+        if (gamePanel != null && gamePanel.getGameState() == GameState.MAIN_MENU) {
+            switch (key) {
+                case KeyEvent.VK_UP:
+                    menuUp = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    menuDown = true;
+                    break;
+                case KeyEvent.VK_ENTER:
+                    menuConfirm = true;
+                    break;
+            }
+            return;
+        }
+
         switch (key) {
             // P1 movement
             case KeyEvent.VK_W: isPressedW = true; break;
@@ -190,6 +212,24 @@ public class GameKeyHandler implements KeyListener {
     public boolean consumeP2Gather() {
         boolean val = p2GatherToggled;
         p2GatherToggled = false;
+        return val;
+    }
+
+    public boolean consumeMenuUp() {
+        boolean val = menuUp;
+        menuUp = false;
+        return val;
+    }
+
+    public boolean consumeMenuDown() {
+        boolean val = menuDown;
+        menuDown = false;
+        return val;
+    }
+
+    public boolean consumeMenuConfirm() {
+        boolean val = menuConfirm;
+        menuConfirm = false;
         return val;
     }
 }
